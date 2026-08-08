292e4b16c146f02bb2376e5f0e10e80f7346f3ca
DodoMaurits <matskooge@protonmail.com>
2026-08-08 18:19:00 +0200

    Update Leiden-Lsingel-script.js

--- Leiden-lopen-singel/Leiden-Lsingel-script.js
+++ Leiden-lopen-singel/Leiden-Lsingel-script.js
@@ -2,61 +2,6 @@ const isResultPage = !!localStorage.getItem("timeTravelResults");
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    /* ----- VERDACHTENGRID ----- */
-    const verdachtenContainer = document.getElementById("verdachtenGrid");
-    if (verdachtenContainer) {
-        verdachtenContainer.innerHTML = `
-            <div class="verdachte" data-id="boomverzorger">
-                <img src="../../icons/De_boomverzorger.png" alt="Verdachte">
-                <p><br>De boomverzorger</p></div>
-
-            <div class="verdachte" data-id="vogeldief">
-                <img src="../../icons/De_vogeldief.png" alt="Verdachte">
-                <p><br>De vogeldief</p></div>
-
-            <div class="verdachte" data-id="vogelspotter">
-                <img src="../../icons/De_vogelspotter.png" alt="Verdachte">
-                <p><br>De vogelspotter</p></div>
-
-            <div class="verdachte" data-id="dakloze">
-                <img src="../../icons/De_dakloze.png" alt="Verdachte">
-                <p><br>De dakloze</p></div>
-
-            <div class="verdachte" data-id="vogelverzorger">
-                <img src="../../icons/De_vogelverzorger.png" alt="Verdachte">
-                <p><br>De vogelverzorger van de volière</p></div>
-
-            <div class="verdachte" data-id="eigenaar">
-                <img src="../../icons/De_eigenaar.png" alt="Verdachte">
-                <p><br>De eigenaar<br>van de muziektent</p></div>
-
-            <div class="verdachte" data-id="portier">
-                <img src="../../icons/De_portier.png" alt="Verdachte">
-                <p><br>De portier<br>van de concertzaal</p></div>
-
-            <div class="verdachte" data-id="schoonmaker">
-                <img src="../../icons/De_schoonmaker.png" alt="Verdachte">
-                <p><br>De schoonmaker<br>van het zwanenbassin</p></div>
-        `;
-    
-        verdachtenContainer.querySelectorAll(".verdachte").forEach(el => {
-            const id = el.dataset.id;
-            if (localStorage.getItem('verdachte-' + id) === 'afgestreept') {
-                el.classList.add('afgestreept');
-            } else {
-                el.classList.remove('afgestreept');
-            }
-            el.addEventListener('click', () => { 
-                el.classList.toggle('afgestreept');
-                if (el.classList.contains('afgestreept')) {
-                    localStorage.setItem('verdachte-' + id, 'afgestreept');
-                } else {
-                    localStorage.removeItem('verdachte-' + id);
-                }
-            });
-        });
-    }
-
     /* ----- JAARTALLENGRID ----- */
     const jaartallenContainer = document.getElementById("jaartallenGrid");
     if (jaartallenContainer) {
