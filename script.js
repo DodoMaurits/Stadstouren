1552697b2888f2e565b1af9c46d2afbcb1851ad8
DodoMaurits <matskooge@protonmail.com>
2026-08-09 19:38:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function() {
     answerInputs.forEach(answerInput => {
         const answerError = answerInput.nextElementSibling;
         if (!answerInput || !answerError) return;
-        const correctAnswers = answerInput.dataset.answer;
+        const correctAnswers = answerInput.dataset.answer
             .toLowerCase()
             .split(',')
             .map(a => a.trim());
