a3a5e3d18eb291616e0de85f0d01b1f093bc89ef
DodoMaurits <matskooge@protonmail.com>
2026-08-09 19:40:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let timerInterval;
 let startTime;
 let finalTime = null;
 
+/* ---------- SPELLINGSAFWIJKINGEN (LEVENSHTEIN) ---------- */
+function levenshtein(a, b) {
+    const matrix = [];
+    for (let i = 0; i <= b.length; i++) matrix[i] = [i];
+    for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            matrix[i][j] = b[i - 1] === a[j - 1]
+                ? matrix[i - 1][j - 1]
+                : Math.min(
+                    matrix[i - 1][j - 1] + 1,
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j] + 1
+                );
+        }
+    }
+    return matrix[b.length][a.length];
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 
     /* ----- INDEX ----- */
@@ -292,22 +311,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
-
-/* ---------- SPELLINGSAFWIJKINGEN (LEVENSHTEIN) ---------- */
-function levenshtein(a, b) {
-    const matrix = [];
-    for (let i = 0; i <= b.length; i++) matrix[i] = [i];
-    for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
-    for (let i = 1; i <= b.length; i++) {
-        for (let j = 1; j <= a.length; j++) {
-            matrix[i][j] = b[i - 1] === a[j - 1]
-                ? matrix[i - 1][j - 1]
-                : Math.min(
-                    matrix[i - 1][j - 1] + 1,
-                    matrix[i][j - 1] + 1,
-                    matrix[i - 1][j] + 1
-                );
-        }
-    }
-    return matrix[b.length][a.length];
-}
